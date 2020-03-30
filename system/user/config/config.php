594eb0d20f6e2d15b6678eda4e162663dae6bad7
danielfox <dfox@repphealth.io>
2020-03-30 13:09:47 -0600

    change permissions

--- system/user/config/config.php
+++ system/user/config/config.php
@@ -10,14 +10,14 @@ $config['encryption_key'] = 'f402be29b5a1a196b8250bfe2d32c6ad38d08467';
 $config['session_crypt_key'] = '613e5048aa345ee5dc5f7a2a6489c7e23f9fb7ea';
 $config['database'] = array(
 	'expressionengine' => array(
-		'hostname' => '127.0.0.1',
-		'database' => 'expression-engine',
-		'username' => 'ee',
-		'password' => 'expression',
+		'hostname' => 'expression-engine.cluster-cinsc7nekapx.us-east-1.rds.amazonaws.com',
+		'database' => 'help_desk',
+		'username' => 'admin',
+		'password' => 'Nvg1hhYXFv2MUIFW4kVD',
 		'dbprefix' => 'exp_',
 		'char_set' => 'utf8mb4',
 		'dbcollat' => 'utf8mb4_unicode_ci',
-		'port'     => ''
+		'port'     => '3306'
 	),
 );
 
